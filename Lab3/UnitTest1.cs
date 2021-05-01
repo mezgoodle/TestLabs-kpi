d03e13827c2e18ceb8d07019b400ed298d10d2fc
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-01 15:51:58 +0300

    Create Lab3 project

---
+++ Lab3/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lab3
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Null(null);
+        }
+    }
+}
