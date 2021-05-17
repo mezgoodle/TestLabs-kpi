77d7405696398516d550ef777eceb8ebe7ffbe6c
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-17 16:07:04 +0300

    Update UnitTest1.cs

--- Lab4/UnitTest1.cs
+++ Lab4/UnitTest1.cs
@@ -1,14 +1,48 @@
 using System;
-using Xunit;
+using IIG.DatabaseConnectionUtils;
+using IIG.CoSFE.DatabaseUtils;
+using IIG.PasswordHashingUtils;
+using System.Text;
 
 namespace Lab4
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(true);
-        }
-    }
+	class Program
+	{
+		private const string Server = @"GOVERLA2";
+		private const string StorageDatabase = @"IIG.CoSWE.StorageDB";
+		private const bool IsTrusted = true;
+		private const string Login = @"coswe";
+		private const string Password = @"L}EjpfCgru9X@GLj";
+		private const int ConnectionTimeout = 75;
+
+		static StorageDatabaseUtils storageDatabase = new(Server, StorageDatabase,
+			IsTrusted, Login, Password, ConnectionTimeout);
+
+		static void Main()
+		{
+			storageDatabase.DeleteFile(1);
+			storageDatabase.DeleteFile(2);
+			storageDatabase.DeleteFile(3);
+			storageDatabase.DeleteFile(4);
+			storageDatabase.DeleteFile(5);
+
+			string array = "Some String";
+
+			byte[] bytes = Encoding.UTF8.GetBytes(array);
+
+			Console.WriteLine(storageDatabase.AddFile("SomeCoolName.txt", bytes));
+
+			Console.WriteLine(storageDatabase.GetFile(6, out string newName,
+				out byte[] newBytes));
+
+			string newArray = Encoding.UTF8.GetString(newBytes);
+			Console.WriteLine(newArray);
+
+			storageDatabase.DeleteFile(1);
+			storageDatabase.DeleteFile(2);
+			storageDatabase.DeleteFile(3);
+			storageDatabase.DeleteFile(4);
+			storageDatabase.DeleteFile(5);
+		}
+	}
 }
