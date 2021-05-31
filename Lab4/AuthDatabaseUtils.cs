c7d3fcbd4cba430d516228777aa0b25d548ab35b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-31 15:09:25 +0300

    Some updates

---
+++ Lab4/AuthDatabaseUtils.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+using IIG.DatabaseConnectionUtils;
+
+namespace IIG.CoSFE.DatabaseUtils
+{
+    public class AuthDatabaseUtils : DatabaseConnection
+    {
+        public AuthDatabaseUtils(NameValueCollection appSettings) : base(appSettings)
+        {
+        }
+
+        public AuthDatabaseUtils(string server, string database, bool isTrusted, string login = null,
+            string password = null, int connectionTimeOut = 15) : base(server, database, isTrusted, login, password,
+            connectionTimeOut)
+        {
+        }
+
+        private bool Credentials(string operation, string login, string password)
+        {
+            if (!ConnectToDatabase())
+                return false;
+
+            bool res;
+
+            using (var cmd = new SqlCommand($"[dbo].[{operation}Credentials]", Connection))
+            {
+                try
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@Login", SqlDbType.NVarChar, 50).Value = login;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar, 64).Value = password;
+                    cmd.Parameters.Add("@Result", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                    cmd.ExecuteNonQuery();
+
+                    res = Convert.ToBoolean(cmd.Parameters["@Result"].Value);
+                }
+                catch
+                {
+                    res = false;
+                }
+                finally
+                {
+                    Connection.Close();
+                }
+            }
+
+            return res;
+        }
+
+        public bool AddCredentials(string login, string password)
+        {
+            return Credentials("Add", login, password);
+        }
+
+        public bool UpdateCredentials(string login, string password, string newLogin, string newPassword)
+        {
+            if (!ConnectToDatabase())
+                return false;
+
+            bool res;
+
+            using (var cmd = new SqlCommand("[dbo].[UpdateCredentials]", Connection))
+            {
+                try
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@Login", SqlDbType.NVarChar, 50).Value = login;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar, 64).Value = password;
+                    cmd.Parameters.Add("@NewLogin", SqlDbType.NVarChar, 50).Value = newLogin;
+                    cmd.Parameters.Add("@NewPassword", SqlDbType.VarChar, 64).Value = newPassword;
+                    cmd.Parameters.Add("@Result", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                    cmd.ExecuteNonQuery();
+
+                    res = Convert.ToBoolean(cmd.Parameters["@Result"].Value);
+                }
+                catch
+                {
+                    res = false;
+                }
+                finally
+                {
+                    Connection.Close();
+                }
+            }
+
+            return res;
+        }
+
+        public bool DeleteCredentials(string login, string password)
+        {
+            return Credentials("Delete", login, password);
+        }
+
+        public bool CheckCredentials(string login, string password)
+        {
+            return Credentials("Check", login, password);
+        }
+    }
+}
--- Lab4/UnitTest1.cs
+++ Lab4/UnitTest1.cs
@@ -1,14 +1,40 @@
 using System;
 using Xunit;
+using IIG.CoSFE.DatabaseUtils;
+using IIG.PasswordHashingUtils;
 
 namespace Lab4
 {
     public class UnitTest1
     {
+        private const string Server = @"GOVERLA2\SQLEXPRESS";
+        private const string AuthDatabase = @"IIG.CoSWE.AuthDB";
+        private const string StorageDatabase = @"IIG.CoSWE.StorageDB";
+        private const bool IsTrusted = true;
+        private const string Login = @"coswe";
+        private const string Password = @"L}EjpfCgru9X@GLj";
+        private const int ConnectionTimeout = 75;
+
+        static readonly AuthDatabaseUtils authDatabase =
+            new(Server, AuthDatabase, IsTrusted,
+                Login, Password, ConnectionTimeout);
+
         [Fact]
         public void Test1()
         {
-            Assert.True(true);
+            string login = "login";
+            string pass = "hashedPass";
+
+            string pass1 = PasswordHasher.GetHash(pass);
+
+            authDatabase.AddCredentials(login, pass1);
+
+            bool areCredentialsTheSame = authDatabase.CheckCredentials(login, pass1);
+
+            authDatabase.DeleteCredentials(login, pass1);
+
+            // Assert
+            Assert.True(areCredentialsTheSame);
         }
     }
 }
