c847f1dcd558b4c5bea4355955a67470ec828827
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-06-03 22:17:13 +0300

    Update IntegrationTests.cs
    Add docstrings

--- Lab4/IntegrationTests.cs
+++ Lab4/IntegrationTests.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using Xunit;
-using IIG.PasswordHashingUtils;
-using IIG.FileWorker;
+﻿using IIG.BinaryFlag;
 using IIG.CoSFE.DatabaseUtils;
-using IIG.BinaryFlag;
+using IIG.FileWorker;
+using IIG.PasswordHashingUtils;
+using Xunit;
 
 namespace Lab4
 {
+    /// <summary>
+    /// Test integration by writing hashed password into file
+    /// </summary>
     public class Test_Integration_With_FileWorker
     {
+        /// <summary>
+        /// Test simple method Write and simple hash
+        /// </summary>
         [Fact]
         public void Write_EmptyHash()
         {
@@ -21,6 +26,9 @@ namespace Lab4
             Assert.Equal(newHash, data);
         }
 
+        /// <summary>
+        /// Test simple method Write and moderate hash
+        /// </summary>
         [Fact]
         public void Write_ModerateHash()
         {
@@ -35,6 +43,9 @@ namespace Lab4
             Assert.Equal(newHash, data);
         }
 
+        /// <summary>
+        /// Test method TryWrite and simple hash
+        /// </summary>
         [Fact]
         public void TryWrite_EmptyHash()
         {
@@ -48,6 +59,9 @@ namespace Lab4
             Assert.Equal(newHash, data);
         }
 
+        /// <summary>
+        /// Test method TryWrite and moderate hash
+        /// </summary>
         [Fact]
         public void TryWrite_ModerateHash()
         {
@@ -63,6 +77,9 @@ namespace Lab4
             Assert.Equal(newHash, data);
         }
 
+        /// <summary>
+        /// Test method TryWrite with 0 attempts to write data
+        /// </summary>
         [Fact]
         public void ZeroTriesWrite_ModerateHash()
         {
@@ -76,6 +93,9 @@ namespace Lab4
             Assert.False(result);
         }
 
+        /// <summary>
+        /// Test method ReadLines and simple hash
+        /// </summary>
         [Fact]
         public void ReadLines_EmptyHash()
         {
@@ -89,6 +109,9 @@ namespace Lab4
             Assert.Equal(newHash, data);
         }
 
+        /// <summary>
+        /// Test method ReadLines and moderate hash
+        /// </summary>
         [Fact]
         public void ReadLines_ModerateHash()
         {
@@ -105,23 +128,29 @@ namespace Lab4
         }
     }
 
+    /// <summary>
+    /// Test integration by storing flags in database
+    /// </summary>
     public class Test_Integration_With_DB
     {
+        /// <summary>
+        /// Config for database
+        /// </summary>
         private const string Server = @"GOVERLA2\SQLEXPRESS";
         private const string BinaryFlagDatabase = @"IIG.CoSWE.FlagpoleDB";
         private const bool IsTrusted = true;
         private const string Login = @"coswe";
         private const string Password = @"L}EjpfCgru9X@GLj";
         private const int ConnectionTimeout = 75;
+        static readonly FlagpoleDatabaseUtils binaryFlagDatabase = new(Server, BinaryFlagDatabase, IsTrusted, Login, Password, ConnectionTimeout);
 
-        static readonly FlagpoleDatabaseUtils binaryFlagDatabase =
-            new(Server, BinaryFlagDatabase, IsTrusted,
-                Login, Password, ConnectionTimeout);
-
+        /// <summary>
+        /// Test UIntConcreteBinaryFlag with True value
+        /// </summary>
         [Fact]
         public void UIntConcreteBinaryFlagTrue()
         {
-            MultipleBinaryFlag actualFlag = new MultipleBinaryFlag(2, true);
+            MultipleBinaryFlag actualFlag = new(2, true);
             binaryFlagDatabase.AddFlag(actualFlag.ToString(), (bool)actualFlag.GetFlag());
             int? flagIDNew = binaryFlagDatabase.GetIntBySql("SELECT MAX(MultipleBinaryFlagID) FROM MultipleBinaryFlags");
             Assert.NotNull(flagIDNew);
@@ -134,10 +163,13 @@ namespace Lab4
             Assert.Null(flagIDEmpty);
         }
 
+        /// <summary>
+        /// Test UIntConcreteBinaryFlag with False value
+        /// </summary>
         [Fact]
         public void UIntConcreteBinaryFlagFalse()
         {
-            MultipleBinaryFlag actualFlag = new MultipleBinaryFlag(2, false);
+            MultipleBinaryFlag actualFlag = new(2, false);
             binaryFlagDatabase.AddFlag(actualFlag.ToString(), (bool)actualFlag.GetFlag());
             int? flagIDNew = binaryFlagDatabase.GetIntBySql("SELECT MAX(MultipleBinaryFlagID) FROM MultipleBinaryFlags");
             Assert.NotNull(flagIDNew);
@@ -150,10 +182,13 @@ namespace Lab4
             Assert.Null(flagIDEmpty);
         }
 
+        /// <summary>
+        /// Test ULongConcreteBinaryFlag with True value
+        /// </summary>
         [Fact]
         public void ULongConcreteBinaryFlagTrue()
         {
-            MultipleBinaryFlag actualFlag = new MultipleBinaryFlag(65 - 1, true);
+            MultipleBinaryFlag actualFlag = new(65 - 1, true);
             binaryFlagDatabase.AddFlag(actualFlag.ToString(), (bool)actualFlag.GetFlag());
             int? flagIDNew = binaryFlagDatabase.GetIntBySql("SELECT MAX(MultipleBinaryFlagID) FROM MultipleBinaryFlags");
             Assert.NotNull(flagIDNew);
@@ -166,10 +201,13 @@ namespace Lab4
             Assert.Null(flagIDEmpty);
         }
 
+        /// <summary>
+        /// Test ULongConcreteBinaryFlag with False value
+        /// </summary>
         [Fact]
         public void ULongConcreteBinaryFlagFalse()
         {
-            MultipleBinaryFlag actualFlag = new MultipleBinaryFlag(65 - 1, false);
+            MultipleBinaryFlag actualFlag = new(65 - 1, false);
             binaryFlagDatabase.AddFlag(actualFlag.ToString(), (bool)actualFlag.GetFlag());
             int? flagIDNew = binaryFlagDatabase.GetIntBySql("SELECT MAX(MultipleBinaryFlagID) FROM MultipleBinaryFlags");
             Assert.NotNull(flagIDNew);
@@ -182,10 +220,13 @@ namespace Lab4
             Assert.Null(flagIDEmpty);
         }
 
+        /// <summary>
+        /// Test UIntArrayConcreteBinaryFlagFalse with True value
+        /// </summary>
         [Fact]
         public void UIntArrayConcreteBinaryFlagTrue()
         {
-            MultipleBinaryFlag actualFlag = new MultipleBinaryFlag(65 + 1, true);
+            MultipleBinaryFlag actualFlag = new(65 + 1, true);
             binaryFlagDatabase.AddFlag(actualFlag.ToString(), (bool)actualFlag.GetFlag());
             int? flagIDNew = binaryFlagDatabase.GetIntBySql("SELECT MAX(MultipleBinaryFlagID) FROM MultipleBinaryFlags");
             Assert.NotNull(flagIDNew);
@@ -198,10 +239,13 @@ namespace Lab4
             Assert.Null(flagIDEmpty);
         }
 
+        /// <summary>
+        /// Test UIntArrayConcreteBinaryFlagFalse with False value
+        /// </summary>
         [Fact]
         public void UIntArrayConcreteBinaryFlagFalse()
         {
-            MultipleBinaryFlag actualFlag = new MultipleBinaryFlag(65 + 1, false);
+            MultipleBinaryFlag actualFlag = new(65 + 1, false);
             binaryFlagDatabase.AddFlag(actualFlag.ToString(), (bool)actualFlag.GetFlag());
             int? flagIDNew = binaryFlagDatabase.GetIntBySql("SELECT MAX(MultipleBinaryFlagID) FROM MultipleBinaryFlags");
             Assert.NotNull(flagIDNew);
